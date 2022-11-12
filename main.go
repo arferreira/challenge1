80834bab14444f04935b553b2eadba9349dd2d5e
Antonio Ricardo <antonio.eschola@gmail.com>
2022-11-12 08:09:39 -0400

    feat: add calculate in main function

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const ebulitonK int = 373
+
+func main(){
+	fmt.Println("Application to Convert from Kelvin to Celsius")
+	ebulitionC := ebulitonK - 273
+	fmt.Printf("The value about conversion is: %d", ebulitionC)
+
+}
